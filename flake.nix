261fed67b075f7e9a9c8ad10998ee63095f5300f
Song Jeyeong <46372718+hooreique@users.noreply.github.com>
2026-08-22 17:25:08 +0900

    simplify flake (#3)

--- flake.nix
+++ flake.nix
@@ -1,63 +1,80 @@
 {
   description = "bash-aware block management for shell rc files";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-  };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs =
-    inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [
-        "aarch64-darwin"
-        "aarch64-linux"
-        "x86_64-linux"
-      ];
-
-      perSystem =
-        { pkgs, ... }:
-        let
-          saseo = pkgs.callPackage ./package.nix { };
-          devPackages = [
-            pkgs.go
-            pkgs.nixfmt
-            pkgs.scdoc
-          ];
-          lintPackages = [
-            pkgs.go-tools
-            pkgs.gosec
-          ];
-        in
-        {
-          packages = {
-            saseo = saseo;
-            default = saseo;
-          };
-
-          devShells = {
-            default = pkgs.mkShell {
-              packages = devPackages;
-            };
-
-            lint = pkgs.mkShell {
-              packages = devPackages ++ lintPackages;
-            };
-          };
-
-          checks.saseo = saseo;
-
-          formatter = pkgs.writeShellApplication {
-            name = "saseo-fmt";
-            runtimeInputs = [
+    { nixpkgs, ... }:
+    let
+      forAllSys =
+        perSys:
+        nixpkgs.lib.genAttrs [ "aarch64-darwin" "aarch64-linux" "x86_64-linux" ] (
+          system:
+          let
+            pkgs = nixpkgs.legacyPackages.${system};
+          in
+          perSys {
+            pkgs = pkgs;
+            saseo = pkgs.callPackage ./package.nix { };
+            dev-pkgs = [
               pkgs.go
               pkgs.nixfmt
+              pkgs.scdoc
             ];
-            text = ''
-              gofmt -w .
-              nixfmt flake.nix package.nix
-            '';
+            lint-pkgs = [
+              pkgs.go-tools
+              pkgs.gosec
+            ];
+          }
+        );
+    in
+    {
+      packages = forAllSys (
+        { saseo, ... }:
+        {
+          saseo = saseo;
+          default = saseo;
+        }
+      );
+
+      devShells = forAllSys (
+        {
+          pkgs,
+          dev-pkgs,
+          lint-pkgs,
+          ...
+        }:
+        {
+          default = pkgs.mkShell {
+            packages = dev-pkgs;
           };
-        };
+
+          lint = pkgs.mkShell {
+            packages = dev-pkgs ++ lint-pkgs;
+          };
+        }
+      );
+
+      checks = forAllSys (
+        { saseo, ... }:
+        {
+          saseo = saseo;
+        }
+      );
+
+      formatter = forAllSys (
+        { pkgs, ... }:
+        pkgs.writeShellApplication {
+          name = "saseo-fmt";
+          runtimeInputs = [
+            pkgs.go
+            pkgs.nixfmt
+          ];
+          text = ''
+            gofmt -w .
+            nixfmt flake.nix package.nix
+          '';
+        }
+      );
     };
 }
